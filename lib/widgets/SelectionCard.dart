8eaf8e6f660a52413a966871515e00d439ce680f
wangkun <wangkun@woodemi.com>
2019-12-06 19:59:27 +0800

    Add category `Creational`
    Signed-off-by: wangkun <wangkun@woodemi.com>

---
+++ lib/widgets/SelectionCard.dart
@@ -0,0 +1,72 @@
+import 'package:design_patterns/constants.dart';
+import 'package:flutter/material.dart';
+
+class SelectionCard extends StatelessWidget {
+  final Color backgroundColor;
+  final String backgroundHeroTag;
+  final Text contentHeader;
+  final Text contentText;
+
+  final double selectionCardWidth = 200.0;
+  final double selectionCardHeight = 120.0;
+  final double selectionCardBorderRadius = 10.0;
+
+  const SelectionCard({
+    @required this.backgroundColor,
+    @required this.backgroundHeroTag,
+    @required this.contentHeader,
+    @required this.contentText,
+    Key key,
+  })  : assert(backgroundColor != null),
+        assert(backgroundHeroTag != null),
+        assert(contentHeader != null),
+        assert(contentText != null),
+        super(key: key);
+
+  BorderRadiusGeometry get _selectionCardBorderRadius =>
+      BorderRadius.circular(selectionCardBorderRadius);
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Material(
+          borderRadius: _selectionCardBorderRadius,
+          elevation: 8.0,
+          child: Hero(
+            tag: backgroundHeroTag,
+            child: Container(
+              decoration: BoxDecoration(
+                color: backgroundColor,
+                borderRadius: _selectionCardBorderRadius,
+              ),
+              width: selectionCardWidth,
+              height: selectionCardHeight,
+            ),
+          ),
+        ),
+        Material(
+          borderRadius: _selectionCardBorderRadius,
+          color: Colors.transparent,
+          child: InkWell(
+            highlightColor: Colors.transparent,
+            splashColor: Colors.black12,
+            borderRadius: _selectionCardBorderRadius,
+            child: Container(
+              padding: const EdgeInsets.all(paddingL),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  contentHeader,
+                  contentText,
+                ],
+              ),
+              height: selectionCardHeight,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
